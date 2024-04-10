39c832833510d785fa98a6dcc4941cd75cfb2327
itsjackie00 <jackiedanso33@gmail.com>
2024-04-10 15:20:00 +0200

    imported main component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 
 <template>
   <HeaderComponents />
+  <MainComponent />
 </template>
 
 <script>
 import HeaderComponents from './components/HeaderComponents.vue';
+import MainComponent from './components/MainComponent.vue';
 
   export default {
     name: 'App',
     components: {
       HeaderComponents,
+      MainComponent
     }
   }
 </script>
