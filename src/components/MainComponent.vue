52ae21b46674211e0492de6f8bb58181689de883
itsjackie00 <jackiedanso33@gmail.com>
2024-04-10 15:21:24 +0200

    created main component with no content

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <div class="container-fluid py-5">
+            <h3> -> Content goes here <- </h3>
+             
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+
+main{
+    background-color: $background-dark;
+
+    h3{
+        color: white;
+    }
+}
+</style>
