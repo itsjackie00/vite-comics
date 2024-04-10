761af4b84e8b34b61d3a4cf38e07ad6f3f914271
itsjackie00 <jackiedanso33@gmail.com>
2024-04-10 15:29:45 +0200

    added content

---
+++ src/components/FooterComponentTop.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer id="footer-top">
+        cioqui
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-3">
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterComponentTop',
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+
+#footer-top{
+    background-color: $background-dark;
+}
+</style>
