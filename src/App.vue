9b3a64d15a665851bcea79efc623abbcdc7b2a13
itsjackie00 <jackiedanso33@gmail.com>
2024-04-10 15:29:36 +0200

    added footertop componet

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
 <template>
   <HeaderComponents />
   <MainComponent />
+  <FooterComponentTop />
 </template>
 
 <script>
 import HeaderComponents from './components/HeaderComponents.vue';
 import MainComponent from './components/MainComponent.vue';
+import FooterComponentTop from './components/FooterComponentTop.vue';
 
   export default {
     name: 'App',
     components: {
       HeaderComponents,
-      MainComponent
+      MainComponent,
+      FooterComponentTop
     }
   }
 </script>
