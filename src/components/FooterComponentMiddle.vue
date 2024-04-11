f70a112a92f25a41a721b3b5a7f2002e0a8df6d4
itsjackie00 <jackiedanso33@gmail.com>
2024-04-11 07:55:47 +0200

    added style and set background img

--- src/components/FooterComponentMiddle.vue
+++ src/components/FooterComponentMiddle.vue
@@ -1,15 +1,28 @@
 <template>
-    <div>
-        ciao
-    </div>
+    <footer id="footer-middle">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
-    export default {
-        name: 'FooterComponentMiddle'
-    }
+export default {
+    name: 'FooterComponentMiddle'
+}
 </script>
 
 <style lang="scss" scoped>
-
+#footer-middle{
+    background-image: url('/img/footer-bg.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 400px;
+    color: white;
+    
+}
 </style>
