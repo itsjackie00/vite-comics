8ea646127a898ee9429c6de06c69e623219038f8
itsjackie00 <jackiedanso33@gmail.com>
2024-04-11 07:47:31 +0200

    imported footer middle

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@
   <HeaderComponents />
   <MainComponent />
   <FooterComponentTop />
+  <FooterComponentMiddle />
 </template>
 
 <script>
 import HeaderComponents from './components/HeaderComponents.vue';
 import MainComponent from './components/MainComponent.vue';
 import FooterComponentTop from './components/FooterComponentTop.vue';
+import FooterComponentMiddle from './components/FooterComponentMiddle.vue';
 
   export default {
     name: 'App',
     components: {
       HeaderComponents,
       MainComponent,
-      FooterComponentTop
+      FooterComponentTop,
+      FooterComponentMiddle
     }
   }
 </script>
