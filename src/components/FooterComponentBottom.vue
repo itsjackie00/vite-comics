389b0d4fb2243c3e0377535a7d927f50046924b5
itsjackie00 <jackiedanso33@gmail.com>
2024-04-11 09:15:09 +0200

    added button

---
+++ src/components/FooterComponentBottom.vue
@@ -0,0 +1,33 @@
+<template>
+    <footer id="footer-bottom">
+        <div class="container">
+            <button type="button" class="btn btn-link">SIGN-UP NOW!</button>
+        </div>
+       
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: 'FooterComponentBottom',
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+
+#footer-bottom{
+    background-color: #303030;
+    height: 80px;
+
+    button{
+        border: 3px solid $primary;
+        color: white;
+        text-decoration: none;
+        font-weight: 600;
+        padding: 10px 20px;
+        border-radius: 0;
+        margin-top: 20px;
+    }
+}
+</style>
