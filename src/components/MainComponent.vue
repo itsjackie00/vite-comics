ef58dbdf2a53bab9c6748dce83dd117f840afda1
itsjackie00 <jackiedanso33@gmail.com>
2024-04-11 17:14:10 +0200

    imported card component

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,25 +1,44 @@
 <template>
     <main>
-        <div class="container py-5">
-            <h3> -> Content goes here <- </h3>
-             
+        <div class="my-container p-2 ">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-3 mb-4" v-for="(item,index) in comics" :key="index">
+                    <CardComponent :thumb="item.thumb" :price="item.price" :series="item.series" />
+                </div>
+            </div>
+
         </div>
     </main>
 </template>
 
 <script>
-    export default {
-        name: 'MainComponent',
+import { comics } from './data/store';
+import CardComponent from './CardComponent.vue';
+
+
+export default {
+    name: 'MainComponent',
+    components: {
+        CardComponent
+    },
+    data() {
+        return {
+            comics: comics
+        }
+    },
+    mounted() {
+        console.log(this.comics)
     }
+}
 </script>
 
 <style lang="scss" scoped>
 @use '../assets/styles/partials/variables' as *;
 
-main{
+main {
     background-color: $background-dark;
 
-    h3{
+    h3 {
         color: white;
     }
 }
