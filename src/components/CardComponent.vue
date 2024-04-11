37e5db3894ec9b30755a15e09f158acbcbdbff78
itsjackie00 <jackiedanso33@gmail.com>
2024-04-11 17:09:33 +0200

    added card components

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <img :src="thumb" class="card-img-top" :alt="series">
+        <div class="card-body">
+            <h5 class="card-title">{{series}}</h5>
+            <p class="card-text">{{ price }}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardComponent',
+    proprs:['thumb','price','series','type'],
+
+
+}
+</script>
+
+<style lang="scss" scoped></style>
