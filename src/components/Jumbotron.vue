cdd1e93bb34782a5ee366db9f82131b472a5373d
itsjackie00 <jackiedanso33@gmail.com>
2024-04-11 19:47:38 +0200

    added button and some scss styles

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,23 +1,48 @@
 <template>
     <div id="jumbohero">
-        <div class="container" >
-            
+        <div class="container">
+            <button type="button" class="btn fs-5">
+                CURRENT SERIES</button>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Jumbotron',
-    }
+export default {
+    name: 'Jumbotron',
+}
 </script>
 
 <style lang="scss" scoped>
-#jumbohero{
+@use '../assets/styles/partials/variables' as *;
+
+#jumbohero {
     background-image: url('/img/jumbotron.jpg');
     background-repeat: no-repeat;
     background-size: cover;
     width: 100%;
     height: 450px;
+    position: relative;
+
+    button {
+        background-color: $primary;
+        color: white;
+        text-decoration: none;
+        font-weight: 600;
+        padding: 10px 20px;
+        border-radius: 0;
+        position: absolute;
+        bottom: -20px;
+        border: 3px solid $primary;
+
+        &:hover {
+            background-color: white;
+            color: $primary;
+            border: 3px solid $primary;
+            transform: scale(1.1);
+            transition: 0.5s;
+        }
+
+    }
 }
 </style>
