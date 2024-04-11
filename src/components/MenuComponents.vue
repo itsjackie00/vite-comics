7cc10280d7624ec8331dc96ba50eed66c84f0f79
itsjackie00 <jackiedanso33@gmail.com>
2024-04-11 09:00:59 +0200

    menu components for footer

---
+++ src/components/MenuComponents.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-4 py-4">
+        <h5>{{ title }}</h5>
+        <ul>
+            <li v-for="(item, index) in items" :key="index">
+                <a :href="item.url">{{ item.text }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MenuComponents',
+    props: ['title', 'items']
+}
+</script>
+
+<style lang="scss" scoped>
+h5 {
+    margin-left: 30px;
+}
+
+li {
+    list-style: none;
+    font-size: smaller;
+
+    a {
+        text-decoration: none;
+        color: grey;
+    }
+}
+</style>
