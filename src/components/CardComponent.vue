431bdfb29456c1b508dbb8dac5e78319c88d593d
itsjackie00 <jackiedanso33@gmail.com>
2024-04-12 06:28:55 +0200

    added style on hover

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -2,7 +2,7 @@
     <div class="my-card mt-5 ">
         <img :src="image" class="card-img-top" :alt="title">
         <div>
-            <h5 id="card-title" class="text-white m-4 text-center">{{title}}</h5>
+            <h5 id="card-title" class="text-white m-4 text-center">{{ title }}</h5>
         </div>
     </div>
 </template>
@@ -10,16 +10,23 @@
 <script>
 export default {
     name: 'CardComponent',
-    props:['image', 'title','text','link'],
+    props: ['image', 'title', 'text', 'link'],
 
 
 }
 </script>
 
 <style lang="scss" scoped>
-    img{
-        height: 15rem;
-        height: 15rem;
-    }
+img {
+    height: 15rem;
+    height: 15rem;
 
+    &:hover {
+        transform: scale(1.1);
+        transition: 0.5s;
+        cursor: pointer;
+        opacity: 0.5;
+        transition: 0.5s;
+    }
+}
 </style>
